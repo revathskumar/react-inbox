fdb46e8e7c415f2fb288144818bcf22679c990ca
Revath S Kumar <rsk@revathskumar.com>
2016-11-29 23:28:51 +0530

    Fetch data and use internal state

--- app/index.js
+++ app/index.js
@@ -4,12 +4,116 @@ import { render } from 'react-dom';
 import {Router, Route, Link, browserHistory} from 'react-router';
 
 class Inbox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mails: [],
+      loading: false,
+      error: {
+        type: '',
+        message: ''
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    fetch('http://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        return res.json();
+      })
+      .then(mails => {
+        this.setState({
+          mails,
+          loading: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          loading: false
+        });
+      })
+  }
+
   render () {
     return (
       <div>
-        <h1> A React Inbox</h1>
+        <ul className="demo-list-icon mdl-list">
+          {
+            this.state.loading ?
+              <Loader />
+            :
+              this.state.mails.map((mail) => {
+                return <ListItem key={mail.id}  {...mail} />;
+              })
+          }
+        </ul>
       </div>
     );
+  }
+}
+
+const ListItem = ({id, title, body}) => {
+  return (
+    <Link to={`/inbox/${id}`} >
+      <li className="mdl-list__item mdl-list__item--three-line">
+        <span className="mdl-list__item-primary-content">
+          <i className="material-icons mdl-list__item-icon">mail</i>
+          <span>{title}</span>
+          <span className="mdl-list__item-text-body">
+            {body}
+          </span>
+        </span>
+      </li>
+    </Link>
+  );
+}
+
+const Loader = () => {
+  return (
+    <div className="mdl-spinner mdl-js-spinner is-active"></div>
+  );
+}
+
+class Mail extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mail: {},
+      loading: false,
+      error: {
+        type: '',
+        message: ''
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    fetch(`http://jsonplaceholder.typicode.com/posts/${this.props.params.id}`)
+      .then(res => {
+        return res.json();
+      })
+      .then(mail => {
+        this.setState({
+          mail,
+          loading: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          loading: false
+        });
+      })
+  }
+  render() {
+    return this.state.loading ? <Loading /> : <ListItem {...this.state.mail} />;
   }
 }
 
@@ -58,7 +162,8 @@ class App extends Component {
   render(
     <Router history={browserHistory} >
       <Route path="/" component={App} >
-        <Route path="/" component={Inbox} />
+        <Route path="/inbox" component={Inbox} />
+        <Route path="/inbox/:id" component={Mail} />
         <Route path="/about" component={About} />
       </Route>
     </Router>
