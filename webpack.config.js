a1a085264265725e56ec581c968f0c32f35c2f59
Revath S Kumar <rsk@revathskumar.com>
2016-12-29 20:20:25 +0530

    fix : Separate vendor and app files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: ["./app/index.js"]
+    app: ["./app/index.js"],
+    vendor: ['react', 'react-dom', 'react-router', 'redux', 'react-redux', 'material-design-lite']
   },
   output: {
     path: path.resolve( __dirname, "dist"),
     publicPath: '/assets/',
-    filename: 'inbox.js'
+    filename: '[name].js'
+  },
+  devServer: {
+    stats: {
+      timings: true,
+      chunkModules: false,
+      chunks: true
+    }
   },
   module: {
     rules: [
@@ -17,5 +26,8 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor')
+  ]
 }
